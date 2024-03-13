1b9fb2325617a12eeb242b17d6d9f8f03df1e076
Quentin Guilloteau <guillo0001@dmi-cl-login.dmi.unibas.ch>
2024-03-13 12:34:01 +0100

    [cc] working python with only scipy

--- benchmark_scripts/connected_components/python/connected_components.py
+++ benchmark_scripts/connected_components/python/connected_components.py
@@ -1,15 +1,16 @@
 import numpy as np
 from scipy.io import mmread
+import scipy.sparse as ss
 import sys
 
-def cc(filename, maxi=20):
+def cc(filename, maxi=40):
     G = mmread(filename)
-    c = np.arange(1.0, float(G.shape[0] + 1), 1.0)
+    c = ss.coo_array(np.arange(1.0, float(G.shape[0] + 1), 1.0))
 
     for iter in range(maxi):
-        x = np.asarray(np.max(G.multiply(np.transpose(c)), axis=1).toarray())
-        c = np.maximum(c, x)
-        print(np.sum(c))
+        x = G.multiply(c.transpose()).max(axis=0)
+        c = c.maximum(x)
+        print(c.sum())
 
 if __name__ == "__main__":
     args = sys.argv
