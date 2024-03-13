b7e89c5dede3d0cc498dc779091d68c7c30896fb
Quentin Guilloteau <Quentin.Guilloteau@unibas.ch>
2024-03-13 08:40:12 +0100

    add script to generate the singularity image of daphne

---
+++ generate_singularity_image.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+set -xe
+
+DEV_IMAGE=daphne-dev.sif
+HERE=$(pwd)
+PATCH_FOLDER=${HERE}/patches
+DAPHNE_COMMIT=5de5748659d21f1e5b63c3d67d150b9cedd68b50 # March 2nd 2024
+
+# download the dev image
+singularity build ${DEV_IMAGE} docker://daphneeu/daphne-dev:2024-03-02_X86-64_BASE_ubuntu20.04
+
+git clone https://github.com/daphne-eu/daphne ${HERE}/daphne-src
+cd ${HERE}/daphne-src
+git checkout ${DAPHNE_COMMIT}
+
+if [ -d "${PATCH_FOLDER}" ]
+  then
+  for patch in "${PATCH_FOLDER}"/*.patch
+  do
+    patch -p1 < ${patch}
+  done
+fi
+
+cd ${HERE}
+singularity exec ${DEV_IMAGE} bash -c "cd daphne-src; ./build.sh --no-deps --installPrefix /usr/local"
