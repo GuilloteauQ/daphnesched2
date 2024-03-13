6cfb1d3b45a6950585b81b9415542d1c3d7ebd92
Quentin Guilloteau <Quentin.Guilloteau@unibas.ch>
2024-03-13 10:26:33 +0100

    [cc] python

---
+++ benchmark_scripts/connected_components/python/connected_components.py
@@ -0,0 +1,17 @@
+import numpy as np
+from scipy.io import mmread
+import sys
+
+def cc(filename, maxi=20):
+    G = mmread(filename)
+    c = np.arange(1.0, float(G.shape[0] + 1), 1.0)
+
+    for iter in range(maxi):
+        x = np.asarray(np.max(G.multiply(np.transpose(c)), axis=1).toarray())
+        c = np.maximum(c, x)
+
+if __name__ == "__main__":
+    args = sys.argv
+    assert len(args) == 2
+    filename = args[1]
+    cc(filename)
