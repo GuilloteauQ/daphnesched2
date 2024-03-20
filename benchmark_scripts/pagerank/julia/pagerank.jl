d682098187d7bccb4e0165c6c476592b74b30359
Quentin Guilloteau <Quentin.Guilloteau@unibas.ch>
2024-03-20 19:05:14 +0100

    [pagerank] julia

---
+++ benchmark_scripts/pagerank/julia/pagerank.jl
@@ -0,0 +1,21 @@
+using MatrixMarket
+
+function pagerank(filename, maxi)
+  G = MatrixMarket.mmread(filename)
+  n = size(G, 1)
+  p = ones(n)
+  alpha = 0.85
+  one_minus_alpha = 1 - alpha
+
+  for iter in 1:maxi
+    p = (G * p) * alpha + p * one_minus_alpha
+    p = p / sum(p)
+  end
+  println(p)
+end
+
+@assert(length(ARGS) == 1)
+filename = ARGS[1]
+maxi = 260
+pagerank(filename, maxi)
+
