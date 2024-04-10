6fd20ddb5b85ab4caefde646afd06c065e782e25
Quentin Guilloteau <Quentin.Guilloteau@unibas.ch>
2024-04-10 10:28:33 +0200

    [bfs] all languages

---
+++ benchmark_scripts/bfs/py/bfs.py
@@ -0,0 +1,24 @@
+import sys
+import time
+import numpy as np
+from scipy.io import mmread
+
+def bfs(filename, maxi=2000):
+    G = mmread(filename)
+    n = G.shape[0]
+    x = np.zeros(n)
+    x[0] = 1.0
+    one = np.ones(n)
+    
+
+    start = time.time()
+    for iter in range(maxi):
+        x = np.minimum(one, x + G.dot(x))
+    end = time.time()
+    print(end - start)
+
+if __name__ == "__main__":
+    args = sys.argv
+    assert len(args) == 2
+    filename = args[1]
+    bfs(filename)
