7cc14049023eb5cec58c9fcc01f3a0b00b002f9b
Quentin Guilloteau <Quentin.Guilloteau@unibas.ch>
2024-04-23 11:03:38 +0200

    [cc][jl][mpi] looks ok

---
+++ benchmark_scripts/connected_components-mpi/jl/connected_components.jl
@@ -0,0 +1,114 @@
+using MatrixMarket
+using SparseArrays
+using SparseMatricesCSR
+
+using MPI
+MPI.Init()
+
+comm = MPI.COMM_WORLD
+
+function read_and_send_matrix(filename)
+  rank = MPI.Comm_rank(comm)
+  world = MPI.Comm_size(comm)
+  if rank == 0
+    G = MatrixMarket.mmread(filename, :csr)
+    n = size(G, 1)
+    nb_sub = world
+    Is = Array{Array{Int}}(undef, nb_sub)
+    Js = Array{Array{Int}}(undef, nb_sub)
+    shapes     = [div(n, nb_sub) for _ in 1:nb_sub]
+    shapes[nb_sub] = n - (nb_sub-1)*div(n, nb_sub)
+    offsets = zeros(nb_sub)
+    offsets[2:nb_sub] = cumsum(shapes)[1:nb_sub-1]
+    for k in 1:nb_sub
+      Is[k] = []
+      Js[k] = []
+    end
+    for row_id in 1:(length(G.rowptr) - 1)
+      next_ptr = G.rowptr[row_id + 1]
+      current_ptr = G.rowptr[row_id]
+      k = div((row_id - 1), div(n, nb_sub)) + 1
+      if k > nb_sub
+        k = nb_sub
+      end
+      nb_elements = next_ptr - current_ptr
+      append!(Is[k], fill(row_id - offsets[k], nb_elements))
+      append!(Js[k], G.colval[current_ptr:next_ptr-1])
+    end
+
+    for k in 2:nb_sub
+      MPI.send(n, comm; dest=k-1, tag=0)
+      MPI.send(Is[k], comm; dest=k-1, tag=1)
+      MPI.send(Js[k], comm; dest=k-1, tag=2)
+      MPI.send(shapes[k], comm; dest=k-1, tag=3)
+    end
+    Gs = sparsecsr(Is[1], Js[1], ones(length(Is[1])), shapes[1], n)
+    return Gs
+  else
+
+    n = MPI.recv(comm; source=0, tag=0)
+    Is = MPI.recv(comm; source=0, tag=1)
+    Js = MPI.recv(comm; source=0, tag=2)
+    shape = MPI.recv(comm; source=0, tag=3)
+    Gs = sparsecsr(Is, Js, ones(length(Is)), shape, n)
+    return Gs
+  end
+end
+
+function G_broadcast_mult_c(G, c)
+  cols = colvals(G)
+  vals = nonzeros(G)
+  m, n = size(G)
+  maxs = zeros(n)
+  @Threads.threads for j = 1:m
+     for i in nzrange(G, j)
+        col = cols[i]
+        val = vals[i]
+        if val * c[j] > maxs[col]
+          maxs[col] = val*c[j]
+        end
+     end
+  end
+  maxs
+end
+
+function cc(filename, maxi)
+  G = read_and_send_matrix(filename)
+  rank = MPI.Comm_rank(comm)
+  world = MPI.Comm_size(comm)
+  n = size(G, 2)
+
+  sizes = [div(n, world) for _ in 1:world]
+  sizes[world] = n - (world-1) * div(n, world)
+  offsets = zeros(Int64, world)
+  offsets[2:world] = cumsum(sizes)[1:world-1]
+
+  start = time_ns()
+  c = vec(collect(1.0:1.0:float(size(G, 2))))
+
+  for iter in 1:maxi
+    x = G_broadcast_mult_c(G, c[1+offsets[rank + 1]:(1+offsets[rank + 1] + sizes[rank + 1] - 1)])
+    c = max.(c, x)
+    if rank < world - 1
+      tmp = MPI.recv(comm; source=rank+1, tag=0)
+      c = max.(c, tmp)
+    end
+    if rank > 0
+      MPI.send(c, comm; dest=rank-1, tag=0)
+    end
+    MPI.Bcast!(c, 0, comm)
+  
+  end
+  fin = time_ns()
+  if rank == 0
+    println((fin - start) * 1e-9)
+    println(sum(c))
+  end
+
+end
+
+@assert(length(ARGS) == 1)
+filename = ARGS[1]
+maxi = 100
+cc(filename, maxi)
+
