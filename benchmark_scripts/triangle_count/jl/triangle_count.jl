40c91c16e162b3bc1b66f7d77d1afcd2f8a283c4
Quentin Guilloteau <Quentin.Guilloteau@unibas.ch>
2024-10-24 16:15:09 +0200

    [tc][jl] reading, compute, result

--- benchmark_scripts/triangle_count/jl/triangle_count.jl
+++ benchmark_scripts/triangle_count/jl/triangle_count.jl
@@ -1,9 +1,12 @@
 using MatrixMarket
 using SparseArrays
 
+start_reading = time_ns()
 G = mmread(ARGS[1])
-start = time_ns()
+start_compute = time_ns()
 G_square = G * G
 nb_triangles = sum(G_square .* G) / 3.0
 fin = time_ns()
-println((fin - start) * 1e-9)
+duration_reading = (fin - start_reading) * 1e-9
+duration_compute = (fin - start_compute) * 1e-9
+println("$(duration_reading),$(duration_compute),$(floor(Int64, nb_triangles))")
