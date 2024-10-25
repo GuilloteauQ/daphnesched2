6b4c79c0f5927caedcc7188b6dfc64ba3cf7362b
Quentin Guilloteau <Quentin.Guilloteau@unibas.ch>
2024-10-25 09:49:30 +0200

    [cpp][bfs] reading, compute, result

--- benchmark_scripts/bfs/cpp/bfs.cpp
+++ benchmark_scripts/bfs/cpp/bfs.cpp
@@ -5,6 +5,7 @@
 #include <Eigen/Core>
 #include <unsupported/Eigen/SparseExtra>
 #include <chrono>
+#include <iomanip>
 
 typedef Eigen::SparseMatrix<double, Eigen::ColMajor> SpMatC;
 typedef Eigen::SparseMatrix<double, Eigen::RowMajor> SpMatR;
@@ -18,23 +19,25 @@ int main(int argc, char** argv) {
 
   int n = atoi(argv[2]);
   int maxi = 200;
+  auto start_reading = std::chrono::high_resolution_clock::now();
 
   SpMatR G(n, n); 
   if (!loadMarket(G, filename)) {
     std::cout << "could  not load mtx file" << std::endl;
     return 1;
   }
+  auto start_compute = std::chrono::high_resolution_clock::now();
 
   Eigen::VectorXd x = Eigen::VectorXd::Zero(n);
   x(0) = 1.0;
   Eigen::VectorXd ones = Eigen::VectorXd::Ones(n);
 
-  auto start = std::chrono::high_resolution_clock::now();
   for (int i = 0; i < maxi; i++) {
     x = ones.array().min((x + G * x).array());
   }
   auto stop = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration<float>(stop - start);
-  std::cout << duration.count() << std::endl;
+  auto duration_compute = std::chrono::duration<float>(stop - start_compute);
+  auto duration_reading = std::chrono::duration<float>(stop - start_reading);
+  std::cout << duration_reading.count() << "," << duration_compute.count() << "," << std::setprecision (16) << x.sum() << std::endl;
   return 0;
 }
