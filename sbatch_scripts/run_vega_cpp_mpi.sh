d50691516da6ae2b12ab3225dbc1e80f87005c29
Quentin Guilloteau <euquenting@vglogin0006.vega.izum.si>
2025-08-12 14:39:09 +0200

    fix sbatch files for mpi (cpp, py, jl)

--- sbatch_scripts/run_vega_cpp_mpi.sh
+++ sbatch_scripts/run_vega_cpp_mpi.sh
@@ -29,27 +29,12 @@ else
   OPTIONS="${CFLAGS} -O3 -DEIGEN_DONT_PARALLELIZE"
 fi
 
-export UCX_TLS="rc,sm,self"    
-# with UCX, only use: 
-    # rc: inter node Infiniband reliable connection
-    # sm: intra node shared memory
-    # self: loopback MPI talking to itself
-
-#export PMIX_MCA_gds="hash"
-# https://github.com/open-mpi/ompi/issues/7516
-# GDS (Global Data Store) component, do not use the ds12 component.
-export PMIX_MCA_gds="^ds12"
-export OMPI_MCA_gds="^ds12" 
-
-# avoid btl fallback with tcp or vader on VEGA --> so crash instead of fallback
-#export OMPI_MCA_btl="^vader,tcp,openib"
-#export PMIX_MCA_btl="^vader,tcp,openib"
-# btl fallback choices for intra node (no tcp/openib set for inter node --> crash)
-export PMIX_MCA_btl="self,vader"
-export OMPI_MCA_btl="self,vader"
+export UCX_TLS=self,sm
+export OMPI_MCA_PML="ucx"
+export PMIX_MCA_gds="hash"
 
 singularity exec --no-mount /cvmfs ${SLURM_SUBMIT_DIR}/jupycpp.sif mpic++ ${SLURM_SUBMIT_DIR}/${SOURCEFILE} -o ${SLURM_SUBMIT_DIR}/${EXECUTABLE} ${OPTIONS}
 
-OMP_NUM_THREADS=${NUM_THREADS} srun --mpi=pmix --cpus-per-task=${NUM_THREADS} singularity exec --no-mount /cvmfs ${SLURM_SUBMIT_DIR}/jupycpp.sif ${SLURM_SUBMIT_DIR}/${EXECUTABLE} ${SLURM_SUBMIT_DIR}/${MATRIX_PATH} ${MATRIX_SIZE} > ${SLURM_SUBMIT_DIR}/${RESULT}
+OMP_NUM_THREADS=${NUM_THREADS} srun --mpi=pmix_v3 --cpus-per-task=${NUM_THREADS} singularity exec ${SLURM_SUBMIT_DIR}/jupycpp.sif ${SLURM_SUBMIT_DIR}/${EXECUTABLE} ${SLURM_SUBMIT_DIR}/${MATRIX_PATH} ${MATRIX_SIZE} > ${SLURM_SUBMIT_DIR}/${RESULT}
 
 exit 0
--- sbatch_scripts/run_vega_jl_mpi.sh
+++ sbatch_scripts/run_vega_jl_mpi.sh
@@ -22,40 +22,12 @@ export MKL_NUM_THREADS=${NUM_THREADS}
 export VECLIB_MAXIMUM_THREADS=${NUM_THREADS}
 export NUMEXPR_NUM_THREADS=${NUM_THREADS}
 
-export OMPI_MCA_PML="ucx"     
-# replace the byte transport layer (BTL) for point-to-point comm 
-# with Unified Communication X (UCX)
-    # PML is either either:
-        # ucx: Unified Communication X with things like
-            # cma/memory: shared memory access via Cross-Memory Attach (CMA) — for inter-process comm on the same node
-            # rc, rc_verbs: Infiniband Reliable Connection
-        # cm: Connection Management PML (delegates all communication to mtl)
-        # ob1: Open Byte Transfer Layer 1 (delegates communication to btl)
+export UCX_TLS=self,sm
+export OMPI_MCA_PML="ucx"
+export PMIX_MCA_gds="hash"
 
-export UCX_TLS="rc,sm,self"    
-# with UCX, only use: 
-    # rc: inter node Infiniband reliable connection
-    # sm: intra node shared memory
-    # self: loopback MPI talking to itself
-
-#export PMIX_MCA_gds="hash"
-# https://github.com/open-mpi/ompi/issues/7516
-# GDS (Global Data Store) component, do not use the ds12 component.
-export PMIX_MCA_gds="^ds12"
-export OMPI_MCA_gds="^ds12" 
-
-# avoid btl fallback with tcp or vader on VEGA --> so crash instead of fallback
-#export OMPI_MCA_btl="^vader,tcp,openib"
-#export PMIX_MCA_btl="^vader,tcp,openib"
-# btl fallback choices for intra node (no tcp/openib set for inter node --> crash)
-export PMIX_MCA_btl="self,vader"
-export OMPI_MCA_btl="self,vader"
-
-srun --cpus-per-task=${NUM_THREADS} singularity exec --no-mount /cvmfs ${SLURM_SUBMIT_DIR}/jupycpp.sif julia --project=$(dirname ${SLURM_SUBMIT_DIR}/${SCRIPT}) -e "using Pkg; Pkg.instantiate()"
-srun --cpus-per-task=${NUM_THREADS} singularity exec --no-mount /cvmfs ${SLURM_SUBMIT_DIR}/jupycpp.sif julia --threads ${NUM_THREADS} --project=$(dirname ${SLURM_SUBMIT_DIR}/${SCRIPT}) ${SLURM_SUBMIT_DIR}/${SCRIPT} ${SLURM_SUBMIT_DIR}/${MATRIX_PATH} > ${SLURM_SUBMIT_DIR}/${RESULT}
-
-# srun --mpi=pmix --cpus-per-task=${NUM_THREADS} singularity exec --no-mount /cvmfs ${SLURM_SUBMIT_DIR}/jupycpp.sif julia --project=$(dirname ${SLURM_SUBMIT_DIR}/${SCRIPT}) -e "using Pkg; Pkg.instantiate()"
-# srun --mpi=pmix --cpus-per-task=${NUM_THREADS} singularity exec --no-mount /cvmfs ${SLURM_SUBMIT_DIR}/jupycpp.sif julia --threads ${NUM_THREADS} --project=$(dirname ${SLURM_SUBMIT_DIR}/${SCRIPT}) ${SLURM_SUBMIT_DIR}/${SCRIPT} ${SLURM_SUBMIT_DIR}/${MATRIX_PATH} > ${SLURM_SUBMIT_DIR}/${RESULT}
+singularity exec --no-mount /cvmfs ${SLURM_SUBMIT_DIR}/jupycpp.sif julia --project=$(dirname ${SLURM_SUBMIT_DIR}/${SCRIPT}) -e "using Pkg; Pkg.instantiate()"
+srun --mpi=pmix_v3 --cpus-per-task=${NUM_THREADS} singularity exec ${SLURM_SUBMIT_DIR}/jupycpp.sif julia --threads ${NUM_THREADS} --project=$(dirname ${SLURM_SUBMIT_DIR}/${SCRIPT}) ${SLURM_SUBMIT_DIR}/${SCRIPT} ${SLURM_SUBMIT_DIR}/${MATRIX_PATH} > ${SLURM_SUBMIT_DIR}/${RESULT}
 
 
 exit 0
